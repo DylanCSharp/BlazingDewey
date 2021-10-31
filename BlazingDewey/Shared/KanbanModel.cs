ed60aab467c0ac7469c04dea31f475cbe5ed47ac
Dylan Cox <dylancox247@gmail.com>
2021-10-31 14:25:25 +0200

    Amending

---
+++ BlazingDewey/Shared/KanbanModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingDewey.Shared
+{
+    public class KanbanModel
+    {
+        public string Id { get; set; }
+        public string CallNumberTitle { get; set; }
+        public string Status { get; set; }
+        public string Summary { get; set; }
+    }
+}
