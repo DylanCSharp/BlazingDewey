41acde1a011a7c429f0e28c50e3530e4eb4c029b
dylancox <dylan.cox@netconfig.co.za>
2021-12-04 17:05:25 +0200

    Tree added

---
+++ BlazingDewey/Shared/Tree/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingDewey.Shared.Tree
+{
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Data { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BlazingDewey/Shared/Tree/TreeDataStructure.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingDewey.Shared.Tree
+{
+    public class TreeDataStructure
+    {
+        public Node Root;
+        public static List<int> CallNumbers = new();
+
+        public TreeDataStructure()
+        {
+            Root = null;
+        }
+
+        public bool Insert(string fileLine)
+        {
+            //Passing in the line from the file and then splitting up the number and description
+            var lineArray = fileLine.Split(':');
+            var number = Convert.ToInt32(lineArray[0]);
+            var description = lineArray[1];
+
+            CallNumbers.Add(number);
+
+            Node before = null;
+            Node after = Root;
+            while (after is not null)
+            {
+                before = after;
+                if (number < after.Data)
+                {
+                    after = after.Left;
+                }
+                else if (number > after.Data)
+                {
+                    after = after.Right;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new();
+            newNode.Data = number;
+            newNode.Description = description;
+            if (Root is null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+                if (number < before.Data)
+                {
+                    before.Left = newNode;
+                }
+                else
+                {
+                    before.Right = newNode;
+                }
+            }
+            return true;
+        }
+
+        public Node Search(int value)
+        {
+            return Search(value, Root);
+        }
+
+        public Node Search(int value, Node parent)
+        {
+            if (parent != null)
+            {
+                if (value == parent.Data)
+                {
+                    return parent;
+                }
+                if (value < parent.Data)
+                {
+                    return Search(value, parent.Left);
+                }
+                else
+                {
+                    return Search(value, parent.Right);
+                }
+            }
+            return null;
+        }
+
+        public void Remove(int value)
+        {
+            Root = Remove(Root, value);
+
+        }
+
+        public Node Remove(Node parent, int key)
+        {
+            if (parent is null)
+            {
+                return parent;
+            }
+            if (key < parent.Data)
+            {
+                parent.Left = Remove(parent.Left, key);
+            }
+            else if (key > parent.Data)
+            {
+                parent.Right = Remove(parent.Right, key);
+            }
+            else
+            {
+                if (parent.Left is null)
+                {
+                    return parent.Right;
+                }
+                else if (parent.Right is null)
+                {
+                    return parent.Left;
+                }
+                parent.Data = MinValue(parent.Right);
+                parent.Right = Remove(parent.Right, parent.Data);
+            }
+            return parent;
+        }
+
+        public static int MinValue(Node node)
+        {
+            int minv = node.Data;
+
+            while (node.Left is not null)
+            {
+                minv = node.Left.Data;
+                node = node.Left;
+            }
+
+            return minv;
+        }
+
+        public void TraversePreOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.WriteLine(parent.Data + " " + parent.Description);
+                TraversePreOrder(parent.Left);
+                TraversePreOrder(parent.Right);
+            }
+        }
+
+        public void TraverseInOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                TraverseInOrder(parent.Left);
+                Console.WriteLine(parent.Data + " " + parent.Description);
+                TraverseInOrder(parent.Right);
+            }
+        }
+
+        public void TraversePostOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                TraversePostOrder(parent.Left);
+                TraversePostOrder(parent.Right);
+                Console.WriteLine(parent.Data + " " + parent.Description);
+            }
+        }
+    }
+}
